4e6d1036290be2527a1e91a1412d49ff0c320da2
yevheniilim <pacmanlim27@gmail.com>
2020-11-24 12:52:54 +0200

    react tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import TodoList from './Todo/todoList';
+import Context from './context'
+import Loader from './loader'
+import Modal from './Modal/modal';
+
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+        .then(response => response.json())
+        .then(todos => {
+          setTimeout(() => {
+            setTodos(todos)
+            setLoading(false)
+          }, 2000)
+          
+        })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(todos.map(item => {
+      if(item.id === id) {
+        item.complited = !item.complited
+      }
+      return item
+    })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(item => item.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      complited: false
+    }]))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{removeTodo:removeTodo}}>
+      <div className="wrapper">
+        <h1>React</h1>
+        <Modal></Modal>
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo}></AddTodo>
+        </React.Suspense>
+        
+        {loading && <Loader></Loader>}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : loading ? null : <p>No Todos</p>}
+       
+      </div>
+    </Context.Provider>
   );
 }
 
